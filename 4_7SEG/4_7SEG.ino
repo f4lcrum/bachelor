b323e226ac4cd04a0b2ddb99ff4d06d251b8449f
Kristian Oravec <xoravec2@fi.muni.cz>
2023-08-04 20:52:09 +0200

    4 7 seg revised

--- 4_7SEG/4_7SEG.ino
+++ 4_7SEG/4_7SEG.ino
@@ -1,74 +1,109 @@
-#define D4 PB11 
-#define pinG PA1
-#define pinC PA0
-#define pinDP PC15 // decimal point
-#define pinD PC14
-#define pinE PC13
+#include <SevSegShift.h>
+
+// shift register pins
+// #define cs PA7 
+// #define data PA6
+// #define latch PA5
 
 #define D1 PA9
-#define pinA PB8
-#define pinF PB7
 #define D2 PA8
 #define D3 PB15
-#define pinB PB6
+#define D4 PB11 
+#define pinA PB8
+#define pinB PA5
+#define pinC PA0
+#define pinD PC13
+#define pinE PC14
+#define pinF PA6
+#define pinG PA1
+#define pinDP PC15 // decimal point
+
+#define BUTTON PB9
+#define DIGITS 4
 #define SEGMENTS 8
+#define TIMER_DURATION 9999 // between 0 and 9999 pls
 
+int dPins[4] = {D1, D2, D3, D4};
+const int clear_seg = 0xff;
 const int pins[SEGMENTS] = {pinA, pinB, pinC, pinD, pinE, pinF, pinG, pinDP};
-const int number_1[SEGMENTS] = {HIGH, LOW, LOW, HIGH, HIGH, HIGH, HIGH, HIGH};
-const int number_2[SEGMENTS] = {LOW, LOW, HIGH, LOW, LOW, HIGH, LOW, HIGH}; 
-const int number_3[SEGMENTS] = {LOW, LOW, LOW, LOW, HIGH, HIGH, LOW, HIGH};
-const int number_4[SEGMENTS] = {HIGH, LOW, LOW, HIGH, HIGH, LOW, LOW, HIGH};
-const int number_5[SEGMENTS] = {LOW, HIGH, LOW, LOW, HIGH, LOW, LOW, HIGH};
-const int number_6[SEGMENTS] = {LOW, HIGH, LOW, LOW, LOW, LOW, LOW, HIGH};
-const int number_7[SEGMENTS] = {LOW, LOW, LOW, HIGH, HIGH, HIGH, HIGH, HIGH};
-const int number_8[SEGMENTS] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, HIGH};
-const int number_9[SEGMENTS] = {LOW, LOW, LOW, LOW, HIGH, LOW, LOW, HIGH};
-const int number_0[SEGMENTS] = {LOW, LOW, LOW, LOW, LOW, LOW, HIGH, HIGH};
-
+const int numbers[10] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};  
+int time_point = millis();
+int timer = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(pinA, OUTPUT);     
-  pinMode(pinB, OUTPUT);     
-  pinMode(pinC, OUTPUT);     
-  pinMode(pinD, OUTPUT);     
-  pinMode(pinE, OUTPUT);     
-  pinMode(pinF, OUTPUT);     
-  pinMode(pinG, OUTPUT);   
-  pinMode(pinDP, OUTPUT);  
+  for (int i = 0; i < SEGMENTS; i++){
+    pinMode(pins[i], OUTPUT);
+  }
+  Serial.begin(9600);
   pinMode(D1, OUTPUT);  
   pinMode(D2, OUTPUT);  
   pinMode(D3, OUTPUT);  
   pinMode(D4, OUTPUT);  
+  set_timer();
+  pinMode(BUTTON, INPUT_PULLUP); 
+}
+
+
+void print_number(int num) {
+  if (num < 0) {
+    num = 0;  
+  }
+  if (num > 9999) {
+    num = 9999;  
+  }
+  print_num(0, num / 1000);
+  num = num % 1000;
+
+  print_num(1, num / 100);
+  num = num % 100;
+
+  print_num(2, num / 10);
+  num = num % 10;
+
+  print_num(3, num / 1);
+  num = num % 1;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  write_D4(); //function call to turn on D1 to write characters for the first digit
-  segment(number_0); // function call to display 3
-  print_decimal(); // function call to display the decimal point
-  delay(1000);
-  segment(number_1);
-  delay(1000);
-  segment(number_2);
-  delay(1000);
-  segment(number_3);
-  delay(1000);
-  segment(number_4);
-  delay(1000);
-  segment(number_5);
-  delay(1000);
-  segment(number_6);
-  delay(1000);
-  segment(number_7);
-  delay(1000);
-  segment(number_8);
-  delay(1000);
-  segment(number_9);
-  delay(1000);
-  
+  int buttonState = digitalRead(BUTTON);
+  if (buttonState == LOW) {
+    set_timer();
+  }
+  print_number(timer);
+  if ((millis() - time_point) > 1000) {
+    time_point = millis();
+    timer--;  
+  }
+ 
 }
 
+void print_num(int id, unsigned char num)
+{
+  cipher_enable(id);
+  for (int i = 0; i < SEGMENTS; i++) {
+    digitalWrite(pins[i], (numbers[num]>>i)&0x01);
+  } 
+  delay(5);
+}
+
+void set_timer() {
+  if (0 > TIMER_DURATION  || TIMER_DURATION > 9999) {
+    return;  
+  }
+  timer = TIMER_DURATION;
+}
+
+void clear_segments() {
+  for (int i = 0; i < SEGMENTS; i++) {
+     digitalWrite(pins[i], HIGH); 
+  }
+}
+
+void cipher_enable(int id) {
+  for (int j = 0; j < 4; j++) digitalWrite(dPins[j], LOW);
+  digitalWrite(dPins[id], HIGH);
+  clear_segments();
+}
 
 void write_D1()
 {
@@ -76,22 +111,26 @@ void write_D1()
   digitalWrite(D2, LOW);
   digitalWrite(D3, LOW);
   digitalWrite(D4, LOW); 
+  clear_segments();
 }
 
 void write_D2()
 {
+ 
   digitalWrite(D1, LOW);
-  digitalWrite(D2, HIGH);
+  digitalWrite(D2, HIGH); 
   digitalWrite(D3, LOW);
-  digitalWrite(D4, LOW); 
+  digitalWrite(D4, LOW);
+  clear_segments();
 }
 
 void write_D3()
 {
   digitalWrite(D1, LOW);
   digitalWrite(D2, LOW);
-  digitalWrite(D3, HIGH);
-  digitalWrite(D4, LOW); 
+  digitalWrite(D3, HIGH); 
+  digitalWrite(D4, LOW);
+  clear_segments();
 }
 
 void write_D4()
@@ -99,19 +138,19 @@ void write_D4()
   digitalWrite(D1, LOW);
   digitalWrite(D2, LOW);
   digitalWrite(D3, LOW);
-  digitalWrite(D4, HIGH); 
+  digitalWrite(D4, HIGH);
+  clear_segments(); 
 }
-
-
 
 void print_decimal() // writing the decimal point
 {
   digitalWrite(pinDP, HIGH);
+  delay(1);
 }
 
-void segment(const int num[]){
+void segment(const int num){
   for (int i = 0; i < SEGMENTS; i++) {
-    digitalWrite(pins[i], num[i]);
+    digitalWrite(pins[i], (numbers[num]>>i)&0x01);
   } 
+  delay(1);
 }
-
